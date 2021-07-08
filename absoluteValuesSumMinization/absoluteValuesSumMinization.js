371e2aeb071a172afb7e8746daa61915ab9e485a
Bernardo <bernardo@ubidots.com>
2021-07-07 19:55:51 -0500

    Install typescript

---
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.js
@@ -0,0 +1,7 @@
+function absoluteValuesSumMinimization(a) {
+    var isEven = a.length;
+}
+// console.log(absoluteValuesSumMinimization([2, 4, 7]));
+// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
