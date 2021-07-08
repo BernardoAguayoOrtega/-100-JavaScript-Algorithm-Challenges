e802aa5502adee2942fcf30a1bffa1ccc05fd5a5
Bernardo <bernardo@ubidots.com>
2021-07-07 19:46:35 -0500

    first challenge absolute Values Sum Minization

---
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -0,0 +1,8 @@
+function absoluteValuesSumMinimization(a: number[]): number {
+    const isEven =
+}
+
+// console.log(absoluteValuesSumMinimization([2, 4, 7]));
+// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
---
+++ ts basics/index.ts
@@ -0,0 +1,6 @@
+function add(num1: number, num2: number): number{
+    return num1 + num2;
+}
+
+console.log(add(3, 4))
+
