dfecfa3ce5854f98b01a8bb2b45baa82fa87e1b0
Bernardo <bernardo@ubidots.com>
2021-07-14 17:43:42 -0500

    add problem

---
+++ add/add.js
@@ -0,0 +1,2 @@
+var addToNumbers = function (number1, number2) { return number1 + number2; };
+console.log(addToNumbers(2, 4))
---
+++ add/add.ts
@@ -0,0 +1,2 @@
+const addToNumbers = (number1:number, number2:number) => number1 + number2
+console.log(addToNumbers(2,4))
