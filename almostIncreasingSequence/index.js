fea467f2474feb0bdd4dc6e84ee79f9e487f2aba
Bernardo <bernardo@ubidots.com>
2021-08-04 20:12:13 -0500

    modified:   index.js

--- almostIncreasingSequence/index.js
+++ almostIncreasingSequence/index.js
@@ -1,18 +1,21 @@
-// Given a sequence of integers as an array, 
-// determine whether it is possible to obtained a strictly 
+// Given a sequence of integers as an array,
+// determine whether it is possible to obtained a strictly
 // increasing sequence by removing no more than one element from the array.
 
-const shouldNotPossible = [1,3,2,1]
-const shouldPossible = [1,2,3,4,5,6,7,8,9,10]
+const shouldNotPossible = [1, 3, 2, 1];
+const shouldPossible = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const isIncreasingSequence = (sequence) => {
-  const isConsecutive = sequence.filter((currentNumber, index) => (currentNumber + 1) !== sequence[index - 1])
-  if(isConsecutive.length === 0){
-    return true
-  } 
-  console.log(isConsecutive.length)
-  return false
-}
+	let changingCounter = 0;
 
-console.log(isIncreasingSequence(shouldNotPossible))
-console.log(isIncreasingSequence(shouldPossible))
+  sequence.forEach((currentNumber, index) => {
+    if (index > 0 && currentNumber < sequence[index - 1]) {
+      changingCounter++;
+    }
+  })
+
+	return changingCounter <= 1;
+};
+
+console.log(isIncreasingSequence(shouldNotPossible));
+console.log(isIncreasingSequence(shouldPossible));
